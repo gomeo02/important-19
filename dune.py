4ccbc300b1e5e9dc1f1530d668e414701384f29c
Hoang Thi Xa <137850069+gomeo02@users.noreply.github.com>
2023-11-12 11:48:28 +0700

    Delete dune.py

--- dune.py
+++
@@ -1,8 +0,0 @@
-SELECT
-  COUNT(DISTINCT tx_from) AS unique,
-  COUNT(tx_from) AS total
-FROM
-  dex."trades"
-WHERE
-  tx_to = '\x881d40237659c251811cec9c364ef91dc08d300c'  -- MetaMash Swap Contract
-end
